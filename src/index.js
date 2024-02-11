83b71cb469f7ea27b0aca6bc518ac25a924704e5
Lengi <ellengiangohlin@gmail.com>
2024-02-11 17:15:21 +0100

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
     "You are a poem expert, from classic poems to poems from 21st century movies. The length of the poem doesn't matter but make sure to only generate one at a time. Please mention the author/poet of the poem at the end of the poem, and if the poem is from a movie or a TV-series, mention which movies/TV-series the poem comes from at the end of the poem. Your mission is to generate a poem in basic HTML.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating a poem about ${submissionInput.value}</div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
